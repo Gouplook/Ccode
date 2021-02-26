7c8ddc015a6bdaa7591ff0d9c96924b0ebdd45a7
yinjinlin <yinjinlin_uplook@163.com>
2021-02-26 17:53:55 +0800

    ..

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-26 17:46:11
+ * @LastEditTime: 2021-02-26 17:52:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Ccode/main.c
+ */
 #include<stdio.h>
 
 int main()
 {
 	printf("hello world");
+	printf("hello world");
+	printf("hello world");
+	printf("hello world");
+	printf("hello world");
+	printf("hello world");
+
+	
+	
 	return 0;
 }
